607f2b6799c86459757e4b49415c9abeff29219f
LAPTOP-00IGEHMU\mugia <mugiaadha@gmail.com>
2024-06-19 15:06:00 +0700

    update send template

--- resources/views/send-template.blade.php
+++ resources/views/send-template.blade.php
@@ -1,24 +1,23 @@
 Assalamualaikum Warahmatullahi Wabarakatuh<br>
 <br>
-Halo {{$name}} 👋<br>
+Kepada Yth {{$name}} 👋<br>
 <br>
-Dengan penuh sukacita dan kebahagiaan, kami dengan rendah hati mengundang Anda untuk berbagi momen spesial dalam rencana pernikahan kami:<br>
+Dengan penuh sukacita dan kebahagiaan, kami dengan rendah hati mengundang Ibu/Bapak/Saudara/I untuk berbagi momen spesial dalam rencana pernikahan kami:<br>
 <br>
 Erika Karenina Rusdaya<br>
 &<br>
 Mugia Adha Kusumah<br>
 <br>
 🎊 Akad akan dilangsungkan pada:<br>
-🗓 Minggu, 08 September 2024<br>
+🗓 Minggu, 01 September 2024<br>
 🕗 08.00 WIB - 09.00 WIB<br>
 <br>
 🥂 Resepsi akan diadakan pada:<br>
-🗓 Minggu, 08 September 2024<br>
-🕗 09.00 WIB - Selesai<br>
+🗓 Minggu, 01 September 2024<br>
+🕗 11.00 WIB - 14.00 WIB<br>
 <br>
-📍 Tempat: (Kabuci) kampung Buyut Cipageran Asih Putera,<br>
-Jl. Kolonel Masturi No.Km. 3, Cipageran, Cimahi Utara,<br>
-Cimahi City, West Java 40511<br>
+📍 Tempat: (Villa Skaters) Jl. Budi no 90, Pasirkaliki, Cimahi Utara, kota Cimahi<br>
+Google maps : https://www.google.com/maps/place/Villa+Skaters/@-6.8787326,107.5613156,17z/data=!3m1!4b1!4m6!3m5!1s0x2e68e5698d6ba78d:0x48ef25a9873eacc6!8m2!3d-6.8787326!4d107.5661865!16s%2Fg%2F11gnpz5n97?entry=ttu<br>
 <br>
 Informasi lengkap acara<br>
 Silakan kunjungi undangan digital kami di:<br>
