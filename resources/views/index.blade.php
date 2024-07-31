32509aa68da65dec9f8aba54069d0cc209386c14
LAPTOP-00IGEHMU\mugia <mugiaadha@gmail.com>
2024-07-31 13:38:41 +0700

    test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,9 +17,9 @@
                         <p style="color:#fff; font-size:16px;">The Wedding Of</p>  
 
                         <br>
-                        <h1 style="color:#fff; font-size:40px">Erika & Mugia</h1>
+                        <h1 style="color:#fff; font-size:50px">Erika & Mugia</h1>
                         <br>
-                        <p style="color:#fff; font-size:18px;">Minggu, 01 September 2024<br>Kepada Yth. Bapak/Ibu/Saudara/i</p>
+                        <p style="color:#fff; font-size:16px;">Minggu, 01 September 2024<br>Kepada Yth. Bapak/Ibu/Saudara/i</p>
                         <br>
                         <h2 style="color:#fff;">{{ $name ?? 'test' }}</h2>
                         <br>
