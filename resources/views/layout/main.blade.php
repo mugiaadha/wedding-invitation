af544914246c2728c540c44165a1b0bd1f4d5e57
LAPTOP-00IGEHMU\mugia <mugiaadha@gmail.com>
2024-07-31 15:50:43 +0700

    test

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
     <!-- <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script> -->
-    <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
+    <link rel="shortcut icon" href="{{ asset('img/bg-about2.png') }}">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" rel="stylesheet"/>
     <link href="{{ asset("css/animate.min.css") }}" rel="stylesheet">
     <link href="{{ asset("css/aos.css") }}" rel="stylesheet">
